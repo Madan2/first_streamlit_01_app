328e3e24711dfe6e0b34b5d62058eeea3dad5ed6
Madan <tnmadan@hotmail.com>
2022-12-31 21:58:54 +0000

    Update streamlit_app.py
    added a new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,9 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-boiled Free-range Egg')
+streamlit.text('Avocado toast')
+
+streamlit.header('Build your own Smoothie')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
