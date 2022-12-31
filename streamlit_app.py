1ac807e0585eacc1d38a4c54f06abdee714f2912
Madan <tnmadan@hotmail.com>
2022-12-31 21:42:48 +0000

    Update streamlit_app.py
    removed emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit
 
 streamlit.title('Breakfast favourites')
-streamlit.header('U+2653Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-boiled Free-range Egg')
