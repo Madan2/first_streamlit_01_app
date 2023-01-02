67f261a3d19a83df1dbeb2c44160079b635fcde2
Madan <tnmadan@hotmail.com>
2023-01-02 20:34:29 +0000

    Update streamlit_app.py
    added emoji's

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,12 @@ import pandas
 
 streamlit.title('My Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled Free-range Egg')
-streamlit.text('Avocado toast')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥬Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-boiled Free-range Egg')
+streamlit.text('🥑🍞Avocado toast')
 
-streamlit.header('Build your own Smoothie')
+streamlit.header('🍊🍍Build your own Smoothie🍇🍎')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
